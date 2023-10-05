35c44e25fbde7dce787b070d3cd46c0635b15221
Cole Weinman <coleweinman@gmail.com>
2023-10-05 10:20:22 -0500

    Initial Commit

---
+++ FairShare/FairShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  FairShareApp.swift
+//  FairShare
+//
+//  Created by Cole Weinman on 10/5/23.
+//
+
+import SwiftUI
+
+@main
+struct FairShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
