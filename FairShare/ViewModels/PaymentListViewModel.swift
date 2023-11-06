7dd455195629545303d9f42738a8feb6bc8bae30
Scott Lai <scottlai@wireless-10-145-17-232.public.utexas.edu>
2023-11-06 17:01:42 -0600

    Sorting and filtering for payments

--- FairShare/ViewModels/PaymentListViewModel.swift
+++ FairShare/ViewModels/PaymentListViewModel.swift
@@ -56,4 +56,46 @@ class PaymentListViewModel: ObservableObject {
                 }
             }
     }
+    
+    func fetchData(uid: String, startDate: Date?, endDate: Date?, minAmount: Double?, maxAmount: Double?, involved: [String]?, sortBy: Sort?, sortOrder: Bool?) {
+        let paymentsRef = db.collection("payments")
+        var query = paymentsRef.whereField("involvedUserIds", arrayContains: uid)
+        if let startDate = startDate, let endDate = endDate {
+            query = query
+                .whereField("date", isGreaterThanOrEqualTo: startDate)
+                .whereField("date", isLessThanOrEqualTo: endDate)
+        }
+        if let minAmount = minAmount, let maxAmount = maxAmount {
+            query = query
+                .whereField("amount", isGreaterThanOrEqualTo: minAmount)
+                .whereField("amount", isLessThanOrEqualTo: maxAmount)
+        }
+        if let involved = involved {
+            query = query.whereField("involved", arrayContainsAny: involved)
+        }
+        
+        if let sortBy = sortBy, let sortOrder = sortOrder {
+            switch sortBy {
+                case .date:
+                    query = query.order(by: "date", descending: !sortOrder)
+                case .amount:
+                    query = query.order(by: "amount", descending: !sortOrder)
+            }
+        }
+        
+        var _ = query.addSnapshotListener { querySnapshot, error in
+            guard let documents = querySnapshot?.documents else {
+                print("Error fetching documents: \(error!)")
+                return
+            }
+            do {
+                let payments = try documents.map { doc in
+                    return try doc.data(as: Payment.self)
+                }
+                self.payments = payments
+            } catch {
+                self.payments = []
+            }
+        }
+    }
 }
