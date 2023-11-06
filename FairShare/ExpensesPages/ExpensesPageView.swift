047aeafe1cbda070e76b171d9d3d112d0e6261be
Scott Lai <scottlai@wireless-10-145-17-232.public.utexas.edu>
2023-11-06 16:33:57 -0600

    Sorting and filtering for expenses

--- FairShare/ExpensesPages/ExpensesPageView.swift
+++ FairShare/ExpensesPages/ExpensesPageView.swift
@@ -7,10 +7,28 @@
 
 import SwiftUI
 
+enum Sort: CaseIterable, Identifiable {
+    var id: Sort { self }
+    case date
+    case amount
+}
+
 struct ExpensesPageView: View {
     @EnvironmentObject var userViewModel: UserViewModel
     @StateObject private var expenseListViewModel = ExpenseListViewModel()
-    @State private var searchText: String = ""
+    @State private var isPresented = false
+    @State private var ascending = true
+    @State private var showDateFilter: Bool = false
+    @State private var showAmountFilter: Bool = false
+    @State private var showPeopleFilter: Bool = false
+    @State private var showSort: Bool = false
+    @State private var startDate = Date()
+    @State private var endDate = Date()
+    @State private var minAmount: String = ""
+    @State private var maxAmount: String = ""
+    @State private var selectedSort: Sort = .date
+    @State private var showAlert = false
+    @State private var alertMessage: String = ""
     
     var pageBackgroundColor: Color = Color(red: 0.933, green: 0.933, blue: 0.933, opacity: 1)
     var cardBackgroundColor: Color = Color(red: 1, green: 1, blue: 1, opacity: 1)
@@ -27,32 +45,134 @@ struct ExpensesPageView: View {
                 VStack(spacing: 16) {
                     HStack(spacing: 12) {
                         Button(action: {
-                            print("tanked")
+                            isPresented.toggle()
                         }) {
-                            Image(systemName: "line.3.horizontal.decrease")
-                                .imageScale(.large)
-                                .foregroundColor(.black)
+                            Spacer()
+                            Text("the button that does the thing")
+                            Spacer()
                         }
                         .frame(maxHeight: .infinity)
                         .padding(8)
                         .background(cardBackgroundColor)
                         .cornerRadius(cardOuterCornerRadius)
-                        TextField("Search...", text: $searchText)
-                            .padding(8)
-                            .background(cardBackgroundColor)
-                            .cornerRadius(cardOuterCornerRadius)
-                            .autocapitalization(.none)
-                        Button(action: {
-                            print("tanked")
-                        }) {
-                            Image(systemName: "slider.horizontal.3")
-                                .imageScale(.large)
-                                .foregroundColor(.black)
+                        .alert(alertMessage, isPresented: $showAlert) {
+                            Button("OK", role: .cancel) {
+                                isPresented = true
+                            }
                         }
-                        .frame(maxHeight: .infinity)
-                        .padding(8)
-                        .background(cardBackgroundColor)
-                        .cornerRadius(cardOuterCornerRadius)
+                        .sheet(isPresented: $isPresented, onDismiss: onDismiss) {
+                            // this is the sheet
+                            VStack {
+                                // TOGGLES
+                                VStack {
+                                    Toggle("Filter by Date", isOn: $showDateFilter)
+                                        .toggleStyle(SwitchToggleStyle(tint: .accentColor))
+                                    Toggle("Filter by Amount", isOn: $showAmountFilter)
+                                        .toggleStyle(SwitchToggleStyle(tint: .accentColor))
+                                    Toggle("Filter by People", isOn: $showPeopleFilter)
+                                        .toggleStyle(SwitchToggleStyle(tint: .accentColor))
+                                    Toggle("Sort", isOn: $showSort)
+                                        .toggleStyle(SwitchToggleStyle(tint: .accentColor))
+                                }
+                                .scenePadding()
+                                // FILTERS
+                                // date filter
+                                if showDateFilter {
+                                    HStack {
+                                        VStack {
+                                            // start date
+                                            Text("Start Date")
+                                            DatePicker(
+                                                selection: $startDate,
+                                                in: ...Date(),
+                                                displayedComponents: .date,
+                                                label: {}
+                                            )
+                                            .scenePadding(.horizontal)
+                                            .scenePadding([.leading, .trailing, .bottom])
+                                        }
+                                        VStack {
+                                            // end date
+                                            Text("End Date")
+                                            DatePicker(
+                                                selection: $endDate,
+                                                in: ...Date(),
+                                                displayedComponents: .date,
+                                                label: {}
+                                            )
+                                            .scenePadding(.horizontal)
+                                            .scenePadding([.leading, .trailing, .bottom])
+                                        }
+                                    }
+                                }
+                                // amount filter
+                                if showAmountFilter {
+                                    HStack {
+                                        VStack {
+                                            // min amount
+                                            Text("Min Amount")
+                                            TextField("0.00", text: $minAmount)
+                                                .scenePadding()
+                                                .background(Color(red: 0.933, green: 0.933, blue: 0.933, opacity: 1))
+                                                .clipShape(RoundedRectangle(cornerRadius: 5.0))
+                                                .scenePadding([.leading, .trailing, .bottom])
+                                        }
+                                        VStack {
+                                            // max amount
+                                            Text("Max Amount")
+                                            TextField("0.00", text: $maxAmount)
+                                                .scenePadding()
+                                                .background(Color(red: 0.933, green: 0.933, blue: 0.933, opacity: 1))
+                                                .clipShape(RoundedRectangle(cornerRadius: 5.0))
+                                                .scenePadding([.leading, .trailing, .bottom])
+                                        }
+                                    }
+                                }
+                                if showPeopleFilter {
+                                    // people filter
+                                    HStack {
+                                        Text("[the thing to filter by people]")
+                                    }
+                                }
+                                if showSort {
+                                    // SORTS
+                                    HStack {
+                                        Text("Sort By")
+                                        Picker("Sort By", selection: $selectedSort) {
+                                            ForEach(Sort.allCases, id: \.self) {sortCase in
+                                                Text(sortCase == .date ? "Date" : "Amount")
+                                                    .tag(sortCase)
+                                            }
+                                        }
+                                        Button(action: {
+                                            ascending.toggle()
+                                        }) {
+                                            // jesus is 0
+                                            // now is a million
+                                            Image(systemName: ascending ? "chevron.up" : "chevron.down")
+                                                .imageScale(.large)
+                                                .foregroundColor(.black)
+                                        }
+                                    }
+                                }
+                                // SUBMIT
+                                HStack {
+                                    // RESET
+                                    Button(action: {
+                                        showDateFilter = false
+                                        showAmountFilter = false
+                                        showPeopleFilter = false
+                                        showSort = false
+                                        ascending = true
+                                        selectedSort = .date
+                                    }) {
+                                        Text("Reset")
+                                    }
+                                    .scenePadding()
+                                }
+                            }
+                            Spacer()
+                        }
                     }
                     .frame(maxWidth: .infinity, maxHeight: 32)
                     if let expenses = expenseListViewModel.expenses {
@@ -89,6 +209,48 @@ struct ExpensesPageView: View {
                 }
             }
         }
+    }
+    
+    // apply the sorts and filters
+    func onDismiss() {
+        isPresented = false
+        
+        // validate the amounts
+        if showAmountFilter && (!isDouble(amount: minAmount) || !isDouble(amount: maxAmount)) {
+            alertMessage = "Enter a valid amount"
+            showAlert = true
+        // validate the dates
+        } else if showDateFilter && (!equalDates(firstDate: startDate, secondDate: endDate) && startDate > endDate) {
+            alertMessage = "Enter a valid date range"
+            showAlert = true
+        }
+        let date1 = showDateFilter ? startDate : nil
+        let date2 = showDateFilter ? endDate : nil
+        let amount1 = showAmountFilter ? Double(minAmount) : nil
+        let amount2 = showAmountFilter ? Double(maxAmount) : nil
+        let sortBy = showSort ? selectedSort : nil
+        let sortOrder = showSort ? ascending : nil
+        if let user = userViewModel.user {
+            expenseListViewModel.fetchData(uid: user.id!, startDate: date1, endDate: date2, minAmount: amount1, maxAmount: amount2, involved: nil, sortBy: sortBy, sortOrder: sortOrder)
+            print("success")
+        } else {
+            print("TANKED")
+        }
+    }
+    
+    func isDouble(amount: String) -> Bool {
+        if let _ = Double(amount) {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func equalDates(firstDate: Date, secondDate: Date) -> Bool {
+        let calendar = Calendar.current
+        let first = calendar.dateComponents([.year, .month, .day], from: firstDate)
+        let second = calendar.dateComponents([.year, .month, .day], from: secondDate)
+        return first == second
     }
 }
 
--- FairShare/ViewModels/ExpenseListViewModel.swift
+++ FairShare/ViewModels/ExpenseListViewModel.swift
@@ -56,4 +56,47 @@ class ExpenseListViewModel: ObservableObject {
                 }
             }
     }
+    
+    func fetchData(uid: String, startDate: Date?, endDate: Date?, minAmount: Double?, maxAmount: Double?, involved: [String]?, sortBy: Sort?, sortOrder: Bool?) {
+        let expensesRef = db.collection("expenses")
+        var query = expensesRef.whereField("involvedUserIds", arrayContains: uid)
+        if let startDate = startDate, let endDate = endDate {
+            query = query
+                .whereField("date", isGreaterThanOrEqualTo: startDate)
+                .whereField("date", isLessThanOrEqualTo: endDate)
+        }
+        if let minAmount = minAmount, let maxAmount = maxAmount {
+            query = query
+                .whereField("totalAmount", isGreaterThanOrEqualTo: minAmount)
+                .whereField("totalAmount", isLessThanOrEqualTo: maxAmount)
+        }
+        if let involved = involved {
+            query = query.whereField("involved", arrayContainsAny: involved)
+        }
+        
+        if let sortBy = sortBy, let sortOrder = sortOrder {
+            switch sortBy {
+                case .date:
+                    query = query.order(by: "date", descending: !sortOrder)
+                case .amount:
+                    query = query.order(by: "totalAmount", descending: !sortOrder)
+            }
+        }
+        
+        var _ = query.addSnapshotListener { querySnapshot, error in
+            guard let documents = querySnapshot?.documents else {
+                print("Error fetching documents: \(error!)")
+                return
+            }
+            do {
+                let expenses = try documents.map { doc in
+                    return try doc.data(as: Expense.self)
+                }
+//                print(expenses)
+                self.expenses = expenses
+            } catch {
+                self.expenses = []
+            }
+        }
+    }
 }
