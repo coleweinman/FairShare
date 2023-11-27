3822fcdfa6e309674aee8c66357f5f5e0be1101e
melodyyin <melody.yin2001@utexas.edu>
2023-11-27 03:29:28 -0600

    New Views for displaying payments and expenses before editing

---
+++ FairShare/ExpensesPages/ViewExpensePage.swift
@@ -0,0 +1,18 @@
+//
+//  ViewExpensePage.swift
+//  FairShare
+//
+//  Created by Melody Yin on 11/25/23.
+//
+
+import SwiftUI
+
+struct ViewExpensePage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ViewExpensePage()
+}
---
+++ FairShare/PaymentsPages/ViewPaymentPage.swift
@@ -0,0 +1,18 @@
+//
+//  ViewPaymentPage.swift
+//  FairShare
+//
+//  Created by Melody Yin on 11/27/23.
+//
+
+import SwiftUI
+
+struct ViewPaymentPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ViewPaymentPage()
+}
